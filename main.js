d2299ce7188dabed4c63abc65f7579f60955031f
Danarquio <danarquio@outlook.com>
2023-05-14 11:11:45 -0400

    DracarnisShop 1.5

--- main.js
+++ main.js
@@ -1,51 +1,83 @@
+alert("¡Muy buen día, Bienvenido a DRACARNIS!\nFabricación artesanal de productos derivados de la carne.\nCecinas🥓 • Charcutería🥩 • Embutidos🌭 • Fiambres🍖");
+
+let nombreUsuario = prompt("Por favor indíquenos su nombre");
+
+alert(`Muy bien ${nombreUsuario}, tenemos diversidad de productos a tu disposición.
+🥩Bondiola
+🌭Salame
+🍖Jamón Serrano
+🥓Charqui
+
+¿Te gustaría ver nuestros precios?`);
 
 
-alert ("¡Muy buen dia, Bienvenido a DRACARNIS!\nFabricación artesanal de productos derivados de la carne.\nCecinas🥓 • Charcutería🥩 • Embutidos🌭 • Fiambres🍖")
 
-let nombreUsuario = prompt("Por favor indiquenos su nombre")
-
-alert ("Muy bien " + nombreUsuario + " tenemos diversidad de productos a tu disposición.\n🥩Bondiola\n🌭Salame\n🍖Jamón Serrano\n🥓Charqui\n\n¿Te gustaria ver nuestros precios?")
-
-let precioBondiola  = 3100
-let precioJamonserrano= 4200
-let precioSalame    = 1990
-let precioCharqui   = 1500
+// mi array de objetos 
+const productos = [
+  { nombre: "🥩Bondiola Curada", precio: 3100, cantidad: 5000, categoria: "curados" }, 
+  { nombre: "🍖Jamón Serrano", precio: 4200, cantidad: 2000, categoria: "curados" },
+  { nombre: "🌭Salame", precio: 1990, cantidad: 8500, categoria: "embutido" },
+  { nombre: "🥓Charqui", precio: 1500, cantidad: 5500, categoria: "deshidratados" }
+];
 
 
 
-function pedirCantidad(producto , precio) {
-let cantidad = 0
-let compra = true
-do {
-    cantidad = prompt("Tenemos " + producto + " en " + precio + "$ los 100gr\npor favor introduzca la cantidad en gramos que desea\nLa cantidad minima a comprar es 100gr\nSi no desea " + producto + " presione CANCELAR" )
+
+//funcion de compra
+function pedirCantidad(producto) {
+  let cantidad = 0;
+  let compra = true;
+
+  do {
+    cantidad = prompt("Tenemos " + producto.nombre + " en " + producto.precio + "$ los 100gr\npor favor introduzca la cantidad en gramos que desea\nLa cantidad minima a comprar es 100gr\nSi no desea " + producto.nombre + " presione CANCELAR");
     if (cantidad === null) { 
-        break
+      break;
     }
 
- if ( cantidad >= 100) {
-    alert("Muchas gracias " + nombreUsuario + " , hemos añadido a tu cesta " + cantidad + " gramos de " + producto + ", por un monto de:\n"+ (( precio * cantidad)/100)+"$")
-    {break}
-} else {
-    alert( "Lo sentimos, la cantidad minima de compra son 100 gramos")
+    if (cantidad >= 100 && cantidad <= producto.cantidad) {
+      alert("Muchas gracias " + nombreUsuario + ", hemos añadido a tu cesta " + cantidad + " gramos de " + producto.nombre + ", por un monto de:\n"+ ((producto.precio * cantidad)/100)+"$");
+      producto.cantidad -= cantidad; 
+      break;
+    } else if (cantidad < 100) {
+      alert("Lo sentimos, la cantidad minima de compra son 100 gramos");
+    } else {
+      alert("Lo sentimos, no tenemos suficiente stock disponible");
+    }
+  } while (compra);
+
+  return cantidad;
 }
-}while (compra)
-return cantidad
-}
-
-
-let cantidadBondiola = pedirCantidad("🥩Bondiola Curada", precioBondiola)
-let cantidadJamonserrano = pedirCantidad("🍖Jamón Serrano", precioJamonserrano)
-let cantidadSalame = pedirCantidad("🌭Salame", precioSalame)
-let cantidadCharqui = pedirCantidad("🥓Charqui", precioCharqui)
 
 
 
-let totalBondiola = (cantidadBondiola * precioBondiola)/100
-let totalJamonserrano = (cantidadJamonserrano * precioJamonserrano)/100
-let totalSalame = (cantidadSalame * precioSalame)/100
-let totalCharqui = (cantidadCharqui * precioCharqui)/100
-let totalapagar = totalBondiola + totalJamonserrano + totalSalame + totalCharqui
 
-alert ( "Su cesta final tiene los siguientes productos\n🥩BONDIOLA CURADA " + cantidadBondiola + "gr " + totalBondiola + "$\n🍖JAMÓN SERRANO " + cantidadJamonserrano + "gr " + totalJamonserrano + "$\n🌭SALAME " + cantidadSalame + "gr " + totalSalame + "$\n🥓CHARQUI " + cantidadCharqui + "gr " + totalCharqui + "$" + "\nTOTAL A PAGAR: " + totalapagar + "$") 
+const cantidades = productos.map((producto) => pedirCantidad(producto) || 0
+);
 
-alert ( "Esperamos que nuestra atención haya sido de su agrado, su producto sera enviado.\n Gracias por comprar en DRACARNIS\n siguenos en instagram @Dracarnis")
+const productosComprados = productos.filter((producto, index) => {
+  return cantidades[index] !== 0;
+});
+
+
+
+const totalaPagar = productosComprados.reduce(
+  (acumulado, producto, index) => {
+    const precio = producto.precio * (cantidades[index] / 100);
+    return acumulado + precio;
+  },
+  0
+);
+
+let resumenCompra = "Su cesta final tiene los siguientes productos:\n";
+
+productosComprados.forEach((producto, index) => {
+  const cantidad = cantidades[index];
+  const total = producto.precio * (cantidad / 100);
+  resumenCompra += `${producto.nombre} ${cantidad} gr ${total}$\n`;
+});
+
+alert(`${resumenCompra}TOTAL A PAGAR: ${totalaPagar}$`);
+
+alert("Esperamos que nuestra atención haya sido de su agrado, su producto será enviado.\nGracias por comprar en DRACARNIS.\nSíguenos en Instagram @Dracarnis"
+);
+
